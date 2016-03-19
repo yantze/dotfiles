806a6d69d25672eec860f1ef766c6df8699e1ccb
yantze <yantze@126.com>
2016-03-20 02:22:40 +0800

    Squashed 'vimrc/' changes from 87f3d24..adc3b9b
    adc3b9b Merge commit '650b744ce130d87ab7cfeb32268038569f952778'
    53013b7 Merge commit '3e9d7db8797b5d7fb6b8a28784d4930268fdb073'
    
    git-subtree-dir: vimrc
    git-subtree-split: adc3b9b26ccae133d8f8c2579bb9de342c872c65

--- script/install_rc.sh
+++ script/install_rc.sh
@@ -2,6 +2,10 @@
 #author: yantze@126.com
 
 
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 650b744ce130d87ab7cfeb32268038569f952778
 # cd "$(dirname $0)/.."
 # dir="$PWD"
 # 
@@ -19,6 +23,28 @@
 # ln -sf "$dir/_vimrc" ~/.vimrc
 # ln -sf "$dir/_gvimrc" ~/.gvimrc
 # ln -sf "$dir/vimfiles" ~/.vim
+<<<<<<< HEAD
+=======
+cd "$(dirname $0)/.."
+dir="$PWD"
+
+time=$(date +%S)
+if [[ -a ~/.vimrc ]]; then
+    mv ~/.vimrc{,_old_$time}
+fi
+if [[ -a ~/.gvimrc ]]; then
+    mv ~/.gvimrc{,_old_$time}
+fi
+if [[ -d ~/.vim ]]; then
+    mv ~/.vim{,_old_$time}
+fi
+
+ln -sf "$dir/_vimrc" ~/.vimrc
+ln -sf "$dir/_gvimrc" ~/.gvimrc
+ln -sf "$dir/vimfiles" ~/.vim
+>>>>>>> 3e9d7db8797b5d7fb6b8a28784d4930268fdb073
+=======
+>>>>>>> 650b744ce130d87ab7cfeb32268038569f952778
 
 #install vundle plugin
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
