650b744ce130d87ab7cfeb32268038569f952778
yantze <yantze@126.com>
2016-03-20 02:02:55 +0800

    Squashed 'vimrc/' changes from cfccf1b..87f3d24
    87f3d24 update local script for mac
    dffc1f0 update composer
    3e9d7db update pt_black.vim address for embed
    
    git-subtree-dir: vimrc
    git-subtree-split: 87f3d24646974963ab61a5e48a2e73266cafe86a

--- script/install_rc.sh
+++ script/install_rc.sh
@@ -2,23 +2,23 @@
 #author: yantze@126.com
 
 
-cd "$(dirname $0)/.."
-dir="$PWD"
-
-time=$(date +%S)
-if [[ -a ~/.vimrc ]]; then
-    mv ~/.vimrc{,_old_$time}
-fi
-if [[ -a ~/.gvimrc ]]; then
-    mv ~/.gvimrc{,_old_$time}
-fi
-if [[ -d ~/.vim ]]; then
-    mv ~/.vim{,_old_$time}
-fi
-
-ln -sf "$dir/_vimrc" ~/.vimrc
-ln -sf "$dir/_gvimrc" ~/.gvimrc
-ln -sf "$dir/vimfiles" ~/.vim
+# cd "$(dirname $0)/.."
+# dir="$PWD"
+# 
+# time=$(date +%S)
+# if [[ -a ~/.vimrc ]]; then
+#     mv ~/.vimrc{,_old_$time}
+# fi
+# if [[ -a ~/.gvimrc ]]; then
+#     mv ~/.gvimrc{,_old_$time}
+# fi
+# if [[ -d ~/.vim ]]; then
+#     mv ~/.vim{,_old_$time}
+# fi
+# 
+# ln -sf "$dir/_vimrc" ~/.vimrc
+# ln -sf "$dir/_gvimrc" ~/.gvimrc
+# ln -sf "$dir/vimfiles" ~/.vim
 
 #install vundle plugin
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
