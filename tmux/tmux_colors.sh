dfa3aa8af422e807643a834b3f1b1680640fb5af
yantze <yantze@126.com>
2017-07-10 20:00:40 +0800

    color

--- tmux/tmux_colors.sh
+++ tmux/tmux_colors.sh
@@ -3,11 +3,11 @@
 # author: hamvocke
 # https://github.com/hamvocke/dotfiles/blob/master/tmux/tmux_colors.sh
 
-for i in {0..255} ; do
-    printf "\x1b[38;5;${i}mcolour${i}\n"
-done
-
-
 # for i in {0..255} ; do
-#   printf "$(tput setab ${i})$(tput setaf 0)colour${i} test test xxxx xxxx xxxx\n"
+#     printf "\x1b[38;5;${i}mcolour${i}\n"
 # done
+
+
+for i in {0..255} ; do
+  printf "$(tput setab ${i})$(tput setaf 0)colour${i} test test xxxx xxxx xxxx\n"
+done
