e36aa5ab8ce5418158663cb6ed311b75e758e2db
yantze <yantze@126.com>
2017-11-28 15:09:22 +0800

    remove go surrend setting

--- vscode/link_settings.sh
+++ vscode/link_settings.sh
@@ -4,3 +4,4 @@ CURRENT_EXECUTE_HOME=$(pwd -P)
 
 ln -s $CURRENT_EXECUTE_HOME/settings.json ~/Library/Application\ Support/Code/User
 ln -s $CURRENT_EXECUTE_HOME/snippets ~/Library/Application\ Support/Code/User
+ln -s $CURRENT_EXECUTE_HOME/keybindings.json ~/Library/Application\ Support/Code/User
