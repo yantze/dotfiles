9b52f0e96db439dfc001d0b839f2e3d48abaf5f3
Zhi Yang <yantze@126.com>
2017-11-29 23:24:53 +0800

    support 1/3 resize window

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,6 +1,3 @@
---[[ pressing the vim-style keys hjkl moves the current window
-     to the left half, lower half, top half, or right half (resp.) ]]
-
 --[[ function factory that takes the multipliers of screen width
 and height to produce the window's x pos, y pos, width, and height ]]
 function baseMove(x, y, w, h)
@@ -10,14 +7,28 @@ function baseMove(x, y, w, h)
         local screen = win:screen()
         local max = screen:frame()
 
-        f.x = max.w * x
+        -- add max.x so it stays on the same screen, works with my second screen
+        f.x = max.w * x + max.x
         f.y = max.h * y
         f.w = max.w * w
         f.h = max.h * h
         win:setFrame(f, 0)
     end
 end
-hs.hotkey.bind({'alt', 'cmd'}, 'H', baseMove(0, 0, 0.5, 1))
-hs.hotkey.bind({'alt', 'cmd'}, 'L', baseMove(0.5, 0, 0.5, 1))
-hs.hotkey.bind({'alt', 'cmd'}, 'J', baseMove(0, 0.5, 1, 0.5))
-hs.hotkey.bind({'alt', 'cmd'}, 'K', baseMove(0, 0, 1, 0.5))
+
+-- feature spectacle/another window sizing apps
+hs.hotkey.bind({'ctrl', 'alt', 'cmd'}, 'Left', baseMove(0, 0, 0.5, 1))
+hs.hotkey.bind({'ctrl', 'alt', 'cmd'}, 'Right', baseMove(0.5, 0, 0.5, 1))
+hs.hotkey.bind({'ctrl', 'alt', 'cmd'}, 'Down', baseMove(0, 0.5, 1, 0.5))
+hs.hotkey.bind({'ctrl', 'alt', 'cmd'}, 'Up', baseMove(0, 0, 1, 0.5))
+hs.hotkey.bind({'ctrl', 'alt', 'cmd'}, '1', baseMove(0, 0, 0.5, 0.5))
+hs.hotkey.bind({'ctrl', 'alt', 'cmd'}, '2', baseMove(0.5, 0, 0.5, 0.5))
+hs.hotkey.bind({'ctrl', 'alt', 'cmd'}, '3', baseMove(0, 0.5, 0.5, 0.5))
+hs.hotkey.bind({'ctrl', 'alt', 'cmd'}, '4', baseMove(0.5, 0.5, 0.5, 0.5))
+hs.hotkey.bind({'ctrl', 'alt', 'cmd'}, 'M', hs.grid.maximizeWindow)
+
+hs.hotkey.bind({'ctrl', 'alt', 'cmd'}, 'H', baseMove(0, 0, 0.5, 1))
+hs.hotkey.bind({'ctrl', 'alt', 'cmd'}, 'L', baseMove(0.5, 0, 0.5, 1))
+hs.hotkey.bind({'ctrl', 'alt', 'cmd'}, 'J', baseMove(0, 0.5, 1, 0.5))
+hs.hotkey.bind({'ctrl', 'alt', 'cmd'}, 'K', baseMove(0, 0, 1, 0.5))
+
