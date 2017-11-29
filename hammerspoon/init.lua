cfa9c3c67bc77758b57ba88240ef55133e03671a
Zhi Yang <yantze@126.com>
2017-11-29 23:21:23 +0800

    add simple first

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,67 +1,23 @@
+--[[ pressing the vim-style keys hjkl moves the current window
+     to the left half, lower half, top half, or right half (resp.) ]]
 
-super = {"ctrl", "alt", "cmd"}
+--[[ function factory that takes the multipliers of screen width
+and height to produce the window's x pos, y pos, width, and height ]]
+function baseMove(x, y, w, h)
+    return function()
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
 
-require "slowq"    -- Avoid accidental cmd-Q
-require "winman"   -- Window management
-require "readline" -- Readline style bindings
--- require "vim"      -- Vim style bindings
--- require "clipboard"
-
--- Meta {{{1
-
-hs.hotkey.bind(super, 'P', function()
-  hs.openPreferences()
-end)
-
-hs.hotkey.bind(super, 'R', function()
-  hs.reload()
-end)
-
-function reloadConfig(files)---- {{{2
-    doReload = false
-    for _,file in pairs(files) do
-        if file:sub(-4) == ".lua" then
-            hs.reload()
-            break
-        end
+        f.x = max.w * x
+        f.y = max.h * y
+        f.w = max.w * w
+        f.h = max.h * h
+        win:setFrame(f, 0)
     end
-end-- }}}2
-
-function uptime()---- {{{2
-
-    local days = hs.execute("uptime | \
-        grep -o '\\d\\+\\sdays' | grep -o '\\d\\+'")
-
-    local seconds = hs.execute("uptime | \
-        grep -o '\\d\\+\\ssecs' | grep -o '\\d\\+'")
-
-    if tonumber(days) then
-        local minutes = hs.execute("uptime | awk '{print $5}' | \
-                sed -e 's/[^0-9:].*//' | sed 's/:/*60+/g' | bc")
-        local minutes = tonumber(minutes) or 0
-        local seconds = tonumber(seconds) or 0
-        local days = tonumber(days) or 0
-        return (days * 24 * 60 + minutes) * 60 + seconds
-    elseif tonumber(seconds) then
-        return tonumber(seconds)
-    else
-        local minutes = hs.execute("uptime | awk '{print $3}' | \
-            sed -e 's/[^0-9:].*//' | sed 's/:/*60+/g' | bc")
-        local minutes = tonumber(minutes) or 0
-        return minutes * 60
-    end
-
-end-- }}}2
-
-hs.pathwatcher.new(os.getenv("HOME") ..
-    "/.hammerspoon/", reloadConfig):start()
-hs.pathwatcher.new(os.getenv("HOME") ..
-    ".homesick/repos/dotfiles/home/.hammerspoon/", reloadConfig):start()
-
-if uptime() > 1000 then
--- I don't want the alert when I have just turned on the computer
-    hs.alert.show("Hammerspoon loaded")
 end
-
--- }}}1
-
+hs.hotkey.bind({'alt', 'cmd'}, 'H', baseMove(0, 0, 0.5, 1))
+hs.hotkey.bind({'alt', 'cmd'}, 'L', baseMove(0.5, 0, 0.5, 1))
+hs.hotkey.bind({'alt', 'cmd'}, 'J', baseMove(0, 0.5, 1, 0.5))
+hs.hotkey.bind({'alt', 'cmd'}, 'K', baseMove(0, 0, 1, 0.5))
