c5b10488f097b4a026d6a07b849e0f0bb3b27a4e
Zhi Yang <yantze@126.com>
2017-12-09 10:49:41 +0800

    add support workflowy for work

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -55,6 +55,7 @@ hs.hotkey.bind({'alt'}, '2', function () hs.application.launchOrFocus("iTerm") e
 -- or try this way: /usr/libexec/PlistBuddy -c 'Print CFBundleIdentifier' /Applications/Safari.app/Contents/Info.plist
 -- more way: https://superuser.com/questions/346369/getting-the-bundle-identifier-of-an-os-x-application-in-a-shell-script
 hs.hotkey.bind({'alt'}, '3', function () hs.application.launchOrFocusByBundleID("com.google.Chrome.app.Default-koegeopamaoljbmhnfjbclbocehhgmkm") end)
+hs.hotkey.bind({'alt'}, '4', function () hs.application.launchOrFocusByBundleID("com.google.Chrome.app.Profile-4-koegeopamaoljbmhnfjbclbocehhgmkm") end)
 hs.hotkey.bind({'ctrl', 'shift'}, 'escape', function () hs.application.launchOrFocus("Activity Monitor") end)
 hs.hotkey.bind({'alt'}, 'e', function () hs.application.launchOrFocus("Finder") end)
 hs.hotkey.bind({'alt'}, 'w', function () hs.application.launchOrFocus("Visual Studio Code") end)
