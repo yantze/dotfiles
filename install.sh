213842d106f187b9115b69b533159908d6fcbdd5
yantze <yantze@126.com>
2018-01-21 22:27:22 +0800

    clone tmux to ~/.local/tmux-build

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
 
 # install tmux lastest manually
-# git clone https://github.com/tmux/tmux.git
+# git clone https://github.com/tmux/tmux.git ~/.local/tmux-build
 # cd tmux
 # sh autogen.sh
 # ./configure && make
