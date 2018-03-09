ccd3b2e706169f91266f5408c74e1e23f0e94d28
yantze <yantze@126.com>
2018-03-09 15:43:17 +0800

    update

--- vscode/link_settings.sh
+++ vscode/link_settings.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
 CURRENT_EXECUTE_HOME=$(pwd -P)
+if [[ `uname` = "Darwin" ]]; then #if mac
+  SETTING_HOME=~/Library/Application\ Support
+else
+  SETTING_HOME=~/.config/Code/User
+fi
 
-ln -s $CURRENT_EXECUTE_HOME/snippets ~/Library/Application\ Support/Code/User
+ln -s $CURRENT_EXECUTE_HOME/snippets $SETTING_HOME/Code/User
+
 
 for file in `ls       \
   *.json              \
