5e9c7972b21465937ef260e97b4f36bda7923c1c
yantze <yantze@126.com>
2018-04-17 19:39:39 +0800

    hammerspoon show console and auto type current browser title and url

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,8 +1,25 @@
 -- require 'clipboard'
 -- require 'slowq'
-require 'vim'
+-- require 'vim'
 -- local vimouse = require('vimouse')
 -- vimouse('cmd', 'm')
+
+-- Define default Spoons which will be loaded later
+if not hspoon_list then
+    hspoon_list = {
+        "ClipShow",
+        "WinWin",
+        "FnMate",
+        "UnsplashZ"
+    }
+end
+
+-- Load those Spoons
+for _, v in pairs(hspoon_list) do
+    hs.loadSpoon(v)
+end
+
+-- 
 
 hs.window.animationDuration = 0 -- disable animations
 hs.grid.setMargins({0, 0})
@@ -109,6 +126,7 @@ hs.hotkey.bind({'alt', 'cmd'}, '\\', toggleMouseModal )
 
 -- lock screen and start screensaver
 -- hs.hotkey.bind({'alt', 'ctrl'}, 'L', function() hs.caffeinate.startScreensaver() end)
+-- hs.caffeinate.lockScreen()
 
 -- sleep system immediately
 hs.hotkey.bind({'alt', 'cmd'}, 'f15', function() hs.caffeinate.systemSleep() end)
@@ -172,7 +190,6 @@ function muteOutput()
     hs.notify.new( {title="Hammerspoon", subTitle="Sound Muted"} ):send()
 end
 hs.hotkey.bind(super, 'o', muteOutput)
-
 -- toggleMicrophoneMute() - toggle mute on the default "Input Device" (probably the microphone).
 --
 function toggleMicrophoneMute()
@@ -189,6 +206,88 @@ hs.hotkey.bind(super, 'i', toggleMicrophoneMute)
 
 -- Do not Disturb
 -- System Preferences -> keyboard shortcuts -> Turn Do Not Disturb On/Off
+
+hs.hotkey.bind({'alt'}, 'tab', function()
+  hs.hints.windowHints()
+end)
+
+----------------------------------------------------------------------------------------------------
+-- Register browser tab typist: Type URL of current tab of running browser in markdown format. i.e. [title](link)
+-- https://github.com/ashfinal/awesome-hammerspoon
+-- Type Browser Link
+-- TODO: url 要用 Shift+Enter 实现
+
+hs.hotkey.bind({'alt', 'command'}, 'v', function()
+  local safari_running = hs.application.applicationsForBundleID("com.apple.Safari")
+  local chrome_running = hs.application.applicationsForBundleID("com.google.Chrome")
+  if #safari_running > 0 then
+    local stat, data = hs.applescript('tell application "Safari" to get {URL, name} of current tab of window 1')
+    if stat then hs.eventtap.keyStrokes("[" .. data[2] .. "](" .. data[1] .. ")") end
+  elseif #chrome_running > 0 then
+    local stat, data = hs.applescript('tell application "Google Chrome" to get {URL, title} of active tab of window 1')
+    if stat then hs.eventtap.keyStrokes("[" .. data[2] .. "](" .. data[1] .. ")") end
+  end
+end)
+
+----------------------------------------------------------------------------------------------------
+-- https://github.com/ashfinal/awesome-hammerspoon
+-- Register Hammerspoon console
+hs.hotkey.bind({'alt'}, 'z', function()
+  hs.toggleConsole()
+end)
+
+
+--[[
+----------------------------------------------------------------------------------------------------
+-- resizeM modal environment
+if spoon.WinWin then
+    spoon.ModalMgr:new("resizeM")
+    local cmodal = spoon.ModalMgr.modal_list["resizeM"]
+    cmodal:bind('', 'escape', 'Deactivate resizeM', function() spoon.ModalMgr:deactivate({"resizeM"}) end)
+    cmodal:bind('', 'Q', 'Deactivate resizeM', function() spoon.ModalMgr:deactivate({"resizeM"}) end)
+    cmodal:bind('', 'tab', 'Toggle Cheatsheet', function() spoon.ModalMgr:toggleCheatsheet() end)
+    cmodal:bind('', 'A', 'Move Leftward', function() spoon.WinWin:stepMove("left") end, nil, function() spoon.WinWin:stepMove("left") end)
+    cmodal:bind('', 'D', 'Move Rightward', function() spoon.WinWin:stepMove("right") end, nil, function() spoon.WinWin:stepMove("right") end)
+    cmodal:bind('', 'W', 'Move Upward', function() spoon.WinWin:stepMove("up") end, nil, function() spoon.WinWin:stepMove("up") end)
+    cmodal:bind('', 'S', 'Move Downward', function() spoon.WinWin:stepMove("down") end, nil, function() spoon.WinWin:stepMove("down") end)
+    cmodal:bind('', 'H', 'Lefthalf of Screen', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("halfleft") end)
+    cmodal:bind('', 'L', 'Righthalf of Screen', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("halfright") end)
+    cmodal:bind('', 'K', 'Uphalf of Screen', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("halfup") end)
+    cmodal:bind('', 'J', 'Downhalf of Screen', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("halfdown") end)
+    cmodal:bind('', 'Y', 'NorthWest Corner', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("cornerNW") end)
+    cmodal:bind('', 'O', 'NorthEast Corner', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("cornerNE") end)
+    cmodal:bind('', 'U', 'SouthWest Corner', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("cornerSW") end)
+    cmodal:bind('', 'I', 'SouthEast Corner', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("cornerSE") end)
+    cmodal:bind('', 'F', 'Fullscreen', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("fullscreen") end)
+    cmodal:bind('', 'C', 'Center Window', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("center") end)
+    cmodal:bind('', '=', 'Stretch Outward', function() spoon.WinWin:moveAndResize("expand") end, nil, function() spoon.WinWin:moveAndResize("expand") end)
+    cmodal:bind('', '-', 'Shrink Inward', function() spoon.WinWin:moveAndResize("shrink") end, nil, function() spoon.WinWin:moveAndResize("shrink") end)
+    cmodal:bind('shift', 'H', 'Move Leftward', function() spoon.WinWin:stepResize("left") end, nil, function() spoon.WinWin:stepResize("left") end)
+    cmodal:bind('shift', 'L', 'Move Rightward', function() spoon.WinWin:stepResize("right") end, nil, function() spoon.WinWin:stepResize("right") end)
+    cmodal:bind('shift', 'K', 'Move Upward', function() spoon.WinWin:stepResize("up") end, nil, function() spoon.WinWin:stepResize("up") end)
+    cmodal:bind('shift', 'J', 'Move Downward', function() spoon.WinWin:stepResize("down") end, nil, function() spoon.WinWin:stepResize("down") end)
+    cmodal:bind('', 'left', 'Move to Left Monitor', function() spoon.WinWin:stash() spoon.WinWin:moveToScreen("left") end)
+    cmodal:bind('', 'right', 'Move to Right Monitor', function() spoon.WinWin:stash() spoon.WinWin:moveToScreen("right") end)
+    cmodal:bind('', 'up', 'Move to Above Monitor', function() spoon.WinWin:stash() spoon.WinWin:moveToScreen("up") end)
+    cmodal:bind('', 'down', 'Move to Below Monitor', function() spoon.WinWin:stash() spoon.WinWin:moveToScreen("down") end)
+    cmodal:bind('', 'space', 'Move to Next Monitor', function() spoon.WinWin:stash() spoon.WinWin:moveToScreen("next") end)
+    cmodal:bind('', '[', 'Undo Window Manipulation', function() spoon.WinWin:undo() end)
+    cmodal:bind('', ']', 'Redo Window Manipulation', function() spoon.WinWin:redo() end)
+    cmodal:bind('', '`', 'Center Cursor', function() spoon.WinWin:centerCursor() end)
+
+    -- Register resizeM with modal supervisor
+    hsresizeM_keys = hsresizeM_keys or {"alt", "R"}
+    if string.len(hsresizeM_keys[2]) > 0 then
+        spoon.ModalMgr.supervisor:bind(hsresizeM_keys[1], hsresizeM_keys[2], "Enter resizeM Environment", function()
+            -- Deactivate some modal environments or not before activating a new one
+            spoon.ModalMgr:deactivateAll()
+            -- Show an status indicator so we know we're in some modal environment now
+            spoon.ModalMgr:activate({"resizeM"}, "#B22222")
+        end)
+    end
+end
+
+--]]
 
 -- TODO hold cmd and move window by mouse
 -- TODO change sound vol
