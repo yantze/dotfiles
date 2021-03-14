09cd0097eb3376f141fb40e5f7342e000e4703e4
yantze <yantze@126.com>
2021-03-14 17:15:24 +0800

    feat: 支持 Fn+上下左右能调整音量和亮度

--- hammerspoon/Spoons/FnMate.spoon/init.lua
+++ hammerspoon/Spoons/FnMate.spoon/init.lua
@@ -24,43 +24,86 @@ function obj:init()
             return true, {hs.eventtap.event.newKeyEvent({"cmd"}, "left", true)}
         elseif event:getFlags()['fn'] and event:getFlags()['cmd'] and event:getCharacters() == "l" then
             return true, {hs.eventtap.event.newKeyEvent({"cmd"}, "right", true)}
-        elseif event:getFlags()['fn'] and event:getCharacters() == "h" then
-            return true, {hs.eventtap.event.newKeyEvent({}, "left", true)}
-        elseif event:getFlags()['fn'] and event:getCharacters() == "l" then
-            return true, {hs.eventtap.event.newKeyEvent({}, "right", true)}
-        elseif event:getFlags()['fn'] and event:getCharacters() == "j" then
-            return true, {hs.eventtap.event.newKeyEvent({}, "down", true)}
-        elseif event:getFlags()['fn'] and event:getCharacters() == "k" then
+        -- elseif event:getFlags()['fn'] and event:getCharacters() == "h" then
+        --     return true, {hs.eventtap.event.newKeyEvent({}, "left", true)}
+        -- elseif event:getFlags()['fn'] and event:getCharacters() == "l" then
+        --     return true, {hs.eventtap.event.newKeyEvent({}, "right", true)}
+        -- elseif event:getFlags()['fn'] and event:getCharacters() == "j" then
+        --     return true, {hs.eventtap.event.newKeyEvent({}, "down", true)}
+        -- elseif event:getFlags()['fn'] and event:getCharacters() == "k" then
             -- hs.alert.show("cmd:" .. tostring(event:getFlags()['cmd']), 0.5)
             -- hs.alert.show("alt:" .. tostring(event:getFlags()['alt']), 0.5)
             -- hs.alert.show("shift:" .. tostring(event:getFlags()['shift']), 0.5)
             -- hs.alert.show("ctrl:" .. tostring(event:getFlags()['ctrl']), 0.5)
-            return true, {hs.eventtap.event.newKeyEvent({}, "up", true)}
+            -- return true, {hs.eventtap.event.newKeyEvent({}, "up", true)}
+
+        -- elseif event:getFlags()['fn'] and event:getKeyCode() == hs.keycodes.map['Lfet'] then
+            -- hs.alert.show(hs.inspect(event:getKeyCode()))
+            -- hs.alert.show(hs.inspect(hs.keycodes.map['Lfet']))
+            -- return true, {hs.eventtap.event.newKeyEvent({"cmd"}, "up", true)}
 
         -- elseif event:getFlags()['fn'] and event:getFlags()['shift'] and event:getCharacters() == "n" then
         --     return true, {hs.eventtap.event.newKeyEvent({'shift'}, "down", true)}
 
-        elseif event:getFlags()['fn'] and event:getCharacters() == "n" then
-            return true, {hs.eventtap.event.newKeyEvent({}, "down", true)}
-        elseif event:getFlags()['fn'] and event:getCharacters() == "p" then
-            return true, {hs.eventtap.event.newKeyEvent({}, "up", true)}
-        elseif event:getFlags()['fn'] and event:getCharacters() == "y" then
-            return true, {hs.eventtap.event.newScrollEvent({10, 0}, {}, "line")}
-        elseif event:getFlags()['fn'] and event:getCharacters() == "o" then
-            return true, {hs.eventtap.event.newScrollEvent({-10, 0}, {}, "line")}
-        elseif event:getFlags()['fn'] and event:getCharacters() == "u" then
-            return true, {hs.eventtap.event.newScrollEvent({0, -10}, {}, "line")}
-        elseif event:getFlags()['fn'] and event:getCharacters() == "i" then
-            return true, {hs.eventtap.event.newScrollEvent({0, 10}, {}, "line")}
-        elseif event:getFlags()['fn'] and event:getCharacters() == "," then
-            local currentpos = hs.mouse.getAbsolutePosition()
-            return true, {hs.eventtap.leftClick(currentpos)}
-        elseif event:getFlags()['fn'] and event:getCharacters() == "." then
-            local currentpos = hs.mouse.getAbsolutePosition()
-            return true, {hs.eventtap.rightClick(currentpos)}
+        -- elseif event:getFlags()['fn'] and event:getCharacters() == "n" then
+        --     return true, {hs.eventtap.event.newKeyEvent({}, "down", true)}
+        -- elseif event:getFlags()['fn'] and event:getCharacters() == "p" then
+        --     return true, {hs.eventtap.event.newKeyEvent({}, "up", true)}
+        -- elseif event:getFlags()['fn'] and event:getCharacters() == "y" then
+        --     return true, {hs.eventtap.event.newScrollEvent({10, 0}, {}, "line")}
+        -- elseif event:getFlags()['fn'] and event:getCharacters() == "o" then
+        --     return true, {hs.eventtap.event.newScrollEvent({-10, 0}, {}, "line")}
+        -- elseif event:getFlags()['fn'] and event:getCharacters() == "u" then
+        --     return true, {hs.eventtap.event.newScrollEvent({0, -10}, {}, "line")}
+        -- elseif event:getFlags()['fn'] and event:getCharacters() == "i" then
+        --     return true, {hs.eventtap.event.newScrollEvent({0, 10}, {}, "line")}
+        -- elseif event:getFlags()['fn'] and event:getCharacters() == "," then
+        --     local currentpos = hs.mouse.getAbsolutePosition()
+        --     return true, {hs.eventtap.leftClick(currentpos)}
+        -- elseif event:getFlags()['fn'] and event:getCharacters() == "." then
+        --     local currentpos = hs.mouse.getAbsolutePosition()
+        --     return true, {hs.eventtap.rightClick(currentpos)}
+
+        -- Functions Key
+        -- elseif event:getFlags()['fn'] then
+        --     hs.alert.show(hs.inspect(event:getKeyCode()))
+        --     return true, { changeVolume(-3) }
+        elseif event:getFlags()['fn'] and event:getKeyCode() == 115 then -- Fn + Left
+            return true, { changeVolume(-3) }
+        elseif event:getFlags()['fn'] and event:getKeyCode() == 119 then -- Fn + Right
+            return true, { changeVolume(3) }
+        elseif event:getFlags()['fn'] and event:getKeyCode() == 116 then -- Fn + Up
+            return true, { changeBright(5) }
+        elseif event:getFlags()['fn'] and event:getKeyCode() == 121 then -- Fn + Down
+            return true, { changeBright(-5) }
         end
     end
     fn_tapper = hs.eventtap.new({hs.eventtap.event.types.keyDown}, catcher):start()
 end
 
+
+function changeVolume(diff)
+    local current = hs.audiodevice.defaultOutputDevice():volume()
+    local new = math.min(100, math.max(0, math.floor(current + diff)))
+    if new > 0 then
+      hs.audiodevice.defaultOutputDevice():setMuted(false)
+    end
+    hs.alert.closeAll(0.0)
+    hs.alert.show("Volume " .. new .. "%", {}, 0.5)
+    hs.audiodevice.defaultOutputDevice():setVolume(new)
+end
+
+function changeBright(diff)
+  local current = hs.brightness.get()
+  local bright = current + diff
+  if (bright > 100) then
+    hs.brightness.set(100)
+  elseif (bright <= 0) then
+    hs.brightness.set(0)
+  else
+    hs.brightness.set(bright)
+  end
+end
+
 return obj
+
